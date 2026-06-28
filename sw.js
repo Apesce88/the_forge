66fc3418aa64e4930cc77f354b18f16f8ef4b20f
Apesce88 <88564097+Apesce88@users.noreply.github.com>
2026-06-28 15:07:40 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // That version mismatch is what triggers the old cache to clear.
 // ============================================================
 
-const CACHE_NAME = 'forge-v9.10';
+const CACHE_NAME = 'forge-v9.11';
 
 // These are the files we pre-cache when the SW first installs.
 // The app can load fully from cache even with no network.
